0d348d913c98ed043a81f635fbef909ac2e46ee8
Mariia S <thimariia@gmail.com>
2024-07-11 19:23:45 -0700

    Assist can open links. Assist has a name Voice.

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import speech_recognition
 import pyaudio
+import webbrowser
 
 sr = speech_recognition.Recognizer()
 mic = speech_recognition.Microphone()
 
 with mic:
     request = sr.listen(source = mic) #voice
-    text = sr.recognize_google(request, language = 'en') #voice -> text 
+    text = sr.recognize_google(request, language = 'en').lower() #voice -> text 
     print(text)
+
+if text == 'open google' or text == 'open google please' or text == 'please open google':
+    print('okay')
+    webbrowser.open('https://www.google.co.uk/')
+
+elif text == 'open youtube' or text == 'please open youtube' or text == 'open youtube please':
+    print('okay')
+    webbrowser.open('https://www.youtube.com/')
+
+elif text == 'voice hello':
+    print('Hello')
+
+
+#webbrowser.open('https://www.youtube.com/')
+
+#assist has a voice 
 
